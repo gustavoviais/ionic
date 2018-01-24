15c084a35661ef9804e686dfe840e18ec68450f2
Gustavo Silva <gustavoviais@gmail.com>
2018-01-23 22:09:30 -0200

    d02c05

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-profile', //apelido
@@ -7,8 +8,17 @@ import { NavController } from 'ionic-angular';
 })
 export class ProfilePage {
 
-    constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
+    }
+
+    showAlert() {
+        let alert = this.alertCtrl.create({
+            title: 'Teste',
+            subTitle: 'Tem gmail?',
+            buttons: ['OK']
+          });
+          alert.present();
     }
 
 }
